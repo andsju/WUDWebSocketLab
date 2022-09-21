542b3e0ec546f3f6e135b4a4d305167df877345a
unknown <anders.sjunnesson@gmail.com>
2022-09-21 10:01:33 +0200

    basic code

---
+++ server.js
@@ -0,0 +1,19 @@
+import { WebSocketServer } from 'ws';
+
+// create WebSocket server
+const wss = new WebSocketServer({port: 8081})
+
+// listen on new connections
+wss.on('connection', (ws) => {
+    // console.log("Hello World");
+    console.log("New client connection");
+    console.log('Number of connected clients: ', wss.clients.size);
+
+    // WebSocket events (ws) for single client
+
+    // close event
+    ws.on('close', () => {
+        console.log('Client disconnected');
+        console.log('Number of remaining connected clients: ', wss.clients.size);
+    });
+});
