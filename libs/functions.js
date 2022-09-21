429794c33744e02d25b551ffed6776a530821498
unknown <anders.sjunnesson@gmail.com>
2022-09-21 11:56:28 +0200

    json data

---
+++ libs/functions.js
@@ -0,0 +1,24 @@
+function parseJSON(data) {
+
+    // try to parse json
+    try {
+        let obj = JSON.parse(data);
+
+        return obj;        
+    } catch (error) {
+        
+        // log to file in real application....
+
+        return {error: "An error receving data...expected json format"};
+    }
+}
+
+function toUpperCases(string) {
+
+}
+
+function randomNumber() {
+
+    return 1;
+}
+export { parseJSON, toUpperCases, randomNumber }
--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 import { WebSocketServer } from 'ws';
 
+// import functions
+import { parseJSON } from './libs/functions.js';
+
 // create WebSocket server
 const wss = new WebSocketServer({port: 8081})
 
 // listen on new connections
 wss.on('connection', (ws) => {
-    // console.log("Hello World");
     console.log("New client connection from IP: ", ws._socket.remoteAddress);
     console.log('Number of connected clients: ', wss.clients.size);
 
@@ -16,4 +18,16 @@ wss.on('connection', (ws) => {
         console.log('Client disconnected');
         console.log('Number of remaining connected clients: ', wss.clients.size);
     });
+
+    // message event
+    ws.on('message', (data) => {
+        console.log('Message received: %s', data);
+
+        // avoid error using try-catch - server still running!
+        // use defined functions to handle errors - better code
+        let obj = parseJSON(data);
+        console.log('obj', obj);
+
+    });
+
 });
