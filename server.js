709ed8b13926aad2b224d8ef002d1f22d7fbe5aa
unknown <anders.sjunnesson@gmail.com>
2022-09-21 10:49:14 +0200

    close event

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const wss = new WebSocketServer({port: 8081})
 // listen on new connections
 wss.on('connection', (ws) => {
     // console.log("Hello World");
-    console.log("New client connection");
+    console.log("New client connection from IP: ", ws._socket.remoteAddress);
     console.log('Number of connected clients: ', wss.clients.size);
 
     // WebSocket events (ws) for single client
