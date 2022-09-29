bcc5f8d32f30ae408e2ff1a32f327a77a74a8b2f
unknown <anders.sjunnesson@gmail.com>
2022-09-29 13:10:09 +0200

    clean up...

--- express-ws-server.js
+++ express-ws-server.js
@@ -38,16 +38,26 @@ const server = http.createServer(app);
 // create WebSocket server - use a predefined server
 const wss = new WebSocketServer({noServer: true});
 
+
+
+/* allow websockets - listener
+------------------------------- */
 // upgrade event - websocket communication
 server.on('upgrade', (req, socket, head) => {
     console.log('Upgrade event client: ', req.headers);
 
     // use authentication - only logged in users allowed ?
 
+    // ...
+    // return;
+
+
 
     // start websocket
     wss.handleUpgrade(req, socket, head, (ws) => {
         console.log("let user use websocket...");
+
+        wss.emit('connection', ws, req);
     });
 
 });
@@ -56,12 +66,45 @@ server.on('upgrade', (req, socket, head) => {
 
 
 
-server.listen(port, (req, res) => {
-    console.log(`Express server (and http) running on port ${port}`);
+/* listen on new connections
+------------------------------- */
+wss.on('connection', (ws) => {
+    console.log("New client connection from IP: ", ws._socket.remoteAddress);
+    console.log('Number of connected clients: ', wss.clients.size);
+
+    // WebSocket events (ws) for single client
+
+    // close event
+    ws.on('close', () => {
+        console.log('Client disconnected');
+        console.log('Number of remaining connected clients: ', wss.clients.size);
+    });
+
+    // message event
+    ws.on('message', (data) => {
+        // console.log('Message received: %s', data);
+
+        let obj = parseJSON(data);
+
+        // message to clients
+        let objBroadcast = {
+            type: "text",
+            msg: obj.msg,
+            nickname: obj.nickname
+        }
+
+        // broadcast to all but this ws...
+        broadcastButExclude(wss, ws, objBroadcast);
+    });
 });
 
 
-// use method listen - server start
-// app.listen(port, (req, res) => {
-//     console.log(`Express server running on port ${port}`);
-// });
+
+
+
+
+/* listen on initial connection
+------------------------------- */
+server.listen(port, (req, res) => {
+    console.log(`Express server (and http) running on port ${port}`);
+});
