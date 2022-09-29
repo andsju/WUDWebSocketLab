d1b8da94f5a9c6466f03477bdd8604a99b1ed161
unknown <anders.sjunnesson@gmail.com>
2022-09-29 11:49:34 +0200

    express and ws combined

---
+++ express-ws-server.js
@@ -0,0 +1,67 @@
+/* dependencies - imports
+------------------------------- */
+import express from 'express';
+
+// core module http - no npm install...
+import http from 'http';
+
+// use websocket server
+import { WebSocketServer } from 'ws';
+
+// import functions
+import { parseJSON, broadcast, broadcastButExclude } from './libs/functions.js';
+
+
+
+/* application variables
+------------------------------- */
+// set port number
+const port = 8081;
+
+
+
+/* express
+------------------------------- */
+// express 'app' environment
+const app = express();
+
+// serve static files - every file in folder named 'public'
+app.use(express.static('public'));
+
+
+/* server
+------------------------------- */
+// use core module http and pass express as an instance
+const server = http.createServer(app); 
+
+
+// create WebSocket server - use a predefined server
+const wss = new WebSocketServer({noServer: true});
+
+// upgrade event - websocket communication
+server.on('upgrade', (req, socket, head) => {
+    console.log('Upgrade event client: ', req.headers);
+
+    // use authentication - only logged in users allowed ?
+
+
+    // start websocket
+    wss.handleUpgrade(req, socket, head, (ws) => {
+        console.log("let user use websocket...");
+    });
+
+});
+
+
+
+
+
+server.listen(port, (req, res) => {
+    console.log(`Express server (and http) running on port ${port}`);
+});
+
+
+// use method listen - server start
+// app.listen(port, (req, res) => {
+//     console.log(`Express server running on port ${port}`);
+// });
